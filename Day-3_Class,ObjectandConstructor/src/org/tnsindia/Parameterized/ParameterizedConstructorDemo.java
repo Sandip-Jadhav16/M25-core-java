e55338b4b8181233240e098c71b25ad64859ddf9
Sandip Jadhav <jadhavsandip1960@gmail.com>
2022-09-28 20:59:57 +0530

    First Commit!!!

---
+++ Day-3_Class,ObjectandConstructor/src/org/tnsindia/Parameterized/ParameterizedConstructorDemo.java
@@ -0,0 +1,35 @@
+package org.tnsindia.Parameterized;
+//Demo on ParameterizedConstructor
+
+
+import java.util.Scanner;
+class Bike
+{
+	//variable
+	public int speed;
+	
+	//Parameterized constructor
+	Bike(int s)
+	{
+		speed=s;
+		System.out.println("The speed is: "+speed+"km/hr");
+	}
+
+	
+	
+	
+}
+public class ParameterizedConstructorDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value of speed: ");
+		int speed=s.nextInt();
+		//object creation
+		Bike b=new Bike(speed);
+		s.close();
+	}
+
+}
