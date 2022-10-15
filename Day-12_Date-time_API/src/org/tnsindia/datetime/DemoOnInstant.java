a6e68bb3c5a89626e04aa899a7383927727f377a
Sandip Jadhav <jadhavsandip1960@gmail.com>
2022-10-15 20:22:02 +0530

    First Commit

---
+++ Day-12_Date-time_API/src/org/tnsindia/datetime/DemoOnInstant.java
@@ -0,0 +1,13 @@
+package org.tnsindia.datetime;
+
+import java.time.Instant;
+
+public class DemoOnInstant {
+
+	public static void main(String[] args) {
+		Instant a=Instant.EPOCH;
+		System.out.println(a);
+
+	}
+
+}
---
+++ Day-12_Date-time_API/src/org/tnsindia/datetime/LocalDateTime_Demo.java
@@ -0,0 +1,22 @@
+package org.tnsindia.datetime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class LocalDateTime_Demo {
+
+	public static void main(String[] args) {
+		//LocalDate d=LocalDate.now();
+				//LocalTime obj=LocalTime.now();
+				LocalDateTime obj1=LocalDateTime.now();
+				LocalDate obj2=LocalDate.of(2022, 10, 15);
+				//System.out.println(d);
+				//System.out.println(obj);
+				
+				
+				System.out.println(obj1);
+				System.out.println(obj2);
+
+	}
+
+}
